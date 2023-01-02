b31ce87a6fb75d5b995cf1f62fd282a26438d780
Sairu <sairufsa@gmail.com>
2023-01-02 16:07:37 -0300

    testing? needs work on

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,31 +5,18 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
-  }));
+  }))
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'AngularCourse'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('AngularCourse');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularCourse app is running!');
-  });
-});
+  it('should create théapp', () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  })
+})
